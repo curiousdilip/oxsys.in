75367d0c3277f74bbedd6a4e828cc61f3a3f1a5a
Dilip Kumar <curiousdilip@gmail.com>
2023-08-11 12:16:05 +0530

    clarity code added

--- app/layout.js
+++ app/layout.js
@@ -51,6 +51,12 @@ export default function RootLayout({ children }) {
 
             gtag('config', 'G-DZ612VHRQ9');`
         }}>
+        </script>
+
+
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "idfn8e2cog");`
+        }}>
 
         </script>
       </head>
