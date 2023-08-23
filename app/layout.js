20e26e1fcb7ccd5dd1ab7f67e63cee20608ff174
Dilip Kumar <curiousdilip@gmail.com>
2023-08-23 14:44:25 +0530

    keyword and schema added

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,8 @@ export default function RootLayout({ children }) {
       <head>
         <title>OXSYS | Compact Medical Oxygen Generators</title>
         <meta name="robots" content="follow, index" />
+        <meta name="keywords" content="oxsys, oxsys.in, oxygen generator, oxsys anand parbat, oxsys india" />
+
         <meta
           name="description"
           content="Founded in 2021, OXSYS excels in Oxygen generation through modular PSA technology. 70% self-manufactured, strong supply chain, medical Oxygen expertise."
@@ -54,6 +56,27 @@ export default function RootLayout({ children }) {
         }}>
         </script>
 
+        <script dangerouslySetInnerHTML={{
+          __html: {
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "url": "https://www.oxsys.in/",
+            "name": "OXSYS | Compact Medical Oxygen Generators",
+            "logo": "https://www.oxsys.in/assets/img/oxsys-logo-black.svg",
+            "description": "Founded in 2021, OXSYS excels in Oxygen generation through modular PSA technology. 70% self-manufactured, strong supply chain, medical Oxygen expertise.",
+            "contactPoint": {
+              "@type": "ContactPoint",
+              "telephone": "91 86043 87421",
+              "contactType": "customer service"
+            },
+            "sameAs": [
+              "https://www.facebook.com/oxsysin",
+              "https://twitter.com/oxsysin/",
+              "https://www.instagram.com/oxsysin/",
+              "https://www.linkedin.com/company/oxsysin/"
+            ]
+          }
+        }} />
         {/* 
         <script dangerouslySetInnerHTML={{
           __html: `(function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "idfn8e2cog");`
