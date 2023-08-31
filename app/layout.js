54838d05ea72f777037907479ef2822750376799
Dilip Kumar <curiousdilip@gmail.com>
2023-08-31 12:15:45 +0530

    schema.org data changed

--- app/layout.js
+++ app/layout.js
@@ -55,27 +55,54 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-DZ612VHRQ9');`
         }}>
         </script>
-        <script type="application/ld+json">
-          {JSON.stringify({
-            "@context": "http://schema.org",
-            "@type": "Organization",
-            "url": "https://www.oxsys.in/",
-            "name": "OXSYS | Compact Medical Oxygen Generators",
-            "logo": "https://www.oxsys.in/assets/img/oxsys-logo-black.svg",
-            "description": "Founded in 2021, OXSYS excels in Oxygen generation through modular PSA technology. 70% self-manufactured, strong supply chain, medical Oxygen expertise.",
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "telephone": "91 86043 87421",
-              "contactType": "customer service"
-            },
-            "sameAs": [
-              "https://www.facebook.com/oxsysin",
-              "https://twitter.com/oxsysin/",
-              "https://www.instagram.com/oxsysin/",
-              "https://www.linkedin.com/company/oxsysin/"
-            ]
-          })}
-        </script>
+        {/* <script type="application/ld+json">
+          {JSON.stringify(
+            {
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "url": "https://www.oxsys.in/",
+              "name": "OXSYS | Compact Medical Oxygen Generators",
+              "logo": "https://www.oxsys.in/assets/img/oxsys-logo-black.svg",
+              "description": "Founded in 2021, OXSYS excels in Oxygen generation through modular PSA technology. 70% self-manufactured, strong supply chain, medical Oxygen expertise.",
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "91 86043 87421",
+                "contactType": "customer service"
+              },
+              "sameAs": [
+                "https://www.facebook.com/oxsysin",
+                "https://twitter.com/oxsysin/",
+                "https://www.instagram.com/oxsysin/",
+                "https://www.linkedin.com/company/oxsysin/"
+              ]
+            }
+          )}
+        </script> */}
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "http://schema.org",
+              "@type": "Organization",
+              "url": "https://www.oxsys.in/",
+              "name": "OXSYS | Compact Medical Oxygen Generators",
+              "logo": "https://www.oxsys.in/assets/img/oxsys-logo-black.svg",
+              "description": "Founded in 2021, OXSYS excels in Oxygen generation through modular PSA technology. 70% self-manufactured, strong supply chain, medical Oxygen expertise.",
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "91 86043 87421",
+                "contactType": "customer service"
+              },
+              "sameAs": [
+                "https://www.facebook.com/oxsysin",
+                "https://twitter.com/oxsysin/",
+                "https://www.instagram.com/oxsysin/",
+                "https://www.linkedin.com/company/oxsysin/"
+              ]
+            })
+          }}
+        />
         {/* 
         <script dangerouslySetInnerHTML={{
           __html: `(function(c,l,a,r,i,t,y){        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);    })(window, document, "clarity", "script", "idfn8e2cog");`
